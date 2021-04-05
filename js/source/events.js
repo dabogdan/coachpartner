b28f5b110445dcd2f64fb6d6f79b5ef9b1875a9c
dabogdan <39666410+dabogdan@users.noreply.github.com>
2021-04-05 17:44:29 +0300

    social icons in the footer are bigger

--- js/source/events.js
+++ js/source/events.js
@@ -1,9 +1,3 @@
-let face = document.querySelectorAll('.team__social-fb');
-let tw = document.querySelectorAll('.team__social-tw');
-// let linkedin = document.getElementsByClassName('team__social-in');
-// let unba = document.getElementsByClassName('team__social-unba');
-// let twContacts = document.querySelector('.contacts__social-tw-icon');
-// let telegaContacts = document.querySelector('.contacts__social-tele-icon');
 let mailContacts = document.querySelector('.contacts__social-mail');
 let menuBox = document.querySelector('.menu__box');
 let menuToggle = document.querySelector('#menu__toggle');
@@ -16,25 +10,8 @@ const orderPackage = document.querySelectorAll('.order-package');
 const overlayPackage = document.getElementById('overlay-package');
 const overlayPackCloseBtn = document.querySelector('.close-button-packages');
 
-const packagesList = document.querySelectorAll('.package-list')
 const packages = document.getElementById('packages');
 
-//
-// twContacts.addEventListener('mouseover', () => {
-//     twContacts.setAttribute('src', 'img/Twitter-orange.svg');
-// })
-//
-// twContacts.addEventListener('mouseout', () => {
-//     twContacts.setAttribute('src', 'img/Twitter-white.svg');
-// })
-//
-// telegaContacts.addEventListener('mouseover', () => {
-//     telegaContacts.setAttribute('src', 'img/Telegram-orange.svg');
-// })
-//
-// telegaContacts.addEventListener('mouseout', () => {
-//     telegaContacts.setAttribute('src', 'img/Telegram-white.svg');
-// })
 
 mailContacts.addEventListener('mouseover', () => {
     mailContacts.setAttribute('src', 'img/mail-orange.svg');
@@ -47,42 +24,6 @@ mailContacts.addEventListener('mouseout', () => {
 menuBox.addEventListener('click', () => {
     menuToggle.checked = '';
 })
-
-// for (let i = 0; i < face.length; i++) {
-//     face[i].addEventListener('mouseover', () => {
-//         face[i].setAttribute('src', 'img/Facebook-orange.svg')
-//     })
-//     face[i].addEventListener('mouseout', () => {
-//         face[i].setAttribute('src', 'img/Facebook.svg')
-//     })
-// }
-//
-// for (let i = 0; i < tw.length; i++) {
-//     tw[i].addEventListener('mouseover', () => {
-//         tw[i].setAttribute('src', 'img/Twitter-orange.svg')
-//     })
-//     tw[i].addEventListener('mouseout', () => {
-//         tw[i].setAttribute('src', 'img/Twitter.svg')
-//     })
-// }
-//
-// for (let i = 0; i < linkedin.length; i++) {
-//     linkedin[i].addEventListener('mouseover', () => {
-//         linkedin[i].setAttribute('src', 'img/Linkedin-orange.svg')
-//     })
-//     linkedin[i].addEventListener('mouseout', () => {
-//         linkedin[i].setAttribute('src', 'img/Linkedin.svg')
-//     })
-// }
-//
-// for (let i = 0; i < unba.length; i++) {
-//     unba[i].addEventListener('mouseover', () => {
-//         unba[i].setAttribute('src', 'img/erau-orange.svg')
-//     })
-//     unba[i].addEventListener('mouseout', () => {
-//         unba[i].setAttribute('src', 'img/erau.svg')
-//     })
-// }
 
 //order button overlay appear
 orderBtn.forEach((e) => {
@@ -102,13 +43,10 @@ orderPackage.forEach((e, i) => {
     e.addEventListener('click', () => {
         overlayPackage.style.display = 'block';
         document.body.style.overflow = 'hidden';
-        console.log(i, packages.options[i]);
         packages.options.forEach((el) => {
             el.removeAttribute('selected');
         })
-
         packages.options[i].setAttribute('selected', '');
-
     })
 })
 
